efe4501e9c4cc6b191a40324e3bbfa855e05ba98
Raghav Kumar <raghkum2000@gmail.com>
2024-07-06 01:18:32 +0530

    terain texture skip fixed

--- FlightSimulator/index.js
+++ FlightSimulator/index.js
@@ -39,7 +39,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmp5q06ti_4.js
+// include: /tmp/tmpz8emqqtd.js
 
   if (!Module.expectedDataFileDownloads) {
     Module.expectedDataFileDownloads = 0;
@@ -240,21 +240,21 @@ Module['FS_createPath']("/shaders", "trees", true, true);
 
   })();
 
-// end include: /tmp/tmp5q06ti_4.js
-// include: /tmp/tmpyldvbjnw.js
+// end include: /tmp/tmpz8emqqtd.js
+// include: /tmp/tmpvowp90yo.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmpyldvbjnw.js
-// include: /tmp/tmpn0pso28e.js
+  // end include: /tmp/tmpvowp90yo.js
+// include: /tmp/tmp5_sp809f.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach(function(task) {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmpn0pso28e.js
+  // end include: /tmp/tmp5_sp809f.js
 
 
 // Sometimes an existing Module object exists with properties
